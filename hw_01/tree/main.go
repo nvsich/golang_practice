f9289ed5010a610d7c3e0ecddc79297ae61a146d
nvsick <nvs.holy@gmail.com>
2024-07-11 23:51:33 +0300

    hw_01 add solution

--- hw_01/tree/main.go
+++ hw_01/tree/main.go
@@ -1,12 +1,98 @@
 package main
 
 import (
-	_ "fmt"
-	_ "io"
+	"fmt"
+	"io"
 	"os"
-	_ "path/filepath"
-	_ "strings"
+	"path/filepath"
 )
+
+type dirNode struct {
+	File     os.DirEntry
+	Children []dirNode
+}
+
+func (d dirNode) String() string {
+	fileInfo, err := d.File.Info()
+	if err != nil {
+		return ""
+	}
+	if fileInfo.IsDir() {
+		return d.File.Name()
+	}
+
+	if fileInfo.Size() != 0 {
+		return fmt.Sprintf("%s (%db)", d.File.Name(), fileInfo.Size())
+	}
+
+	return fmt.Sprintf("%s (empty)", d.File.Name())
+}
+
+func getDirNodes(rootPath string, printFiles bool) ([]dirNode, error) {
+	files, err := os.ReadDir(rootPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var dirNodes []dirNode
+
+	for _, file := range files {
+		if !file.IsDir() && !printFiles {
+			continue
+		}
+
+		node := dirNode{file, nil}
+
+		if file.IsDir() {
+			children, err := getDirNodes(filepath.Join(rootPath, file.Name()), printFiles)
+			if err != nil {
+				return nil, err
+			}
+
+			node.Children = children
+		}
+
+		dirNodes = append(dirNodes, node)
+	}
+
+	return dirNodes, nil
+}
+
+func printNodes(out io.Writer, dirNodes []dirNode, parentPrefix string) {
+	var (
+		currentPrefix = "├───"
+		childPrefix   = "│\t"
+	)
+
+	for i, node := range dirNodes {
+		if i == len(dirNodes)-1 {
+			currentPrefix = "└───"
+			childPrefix = "\t"
+		}
+
+		formattedName := parentPrefix + currentPrefix + node.String() + "\n"
+
+		_, err := out.Write([]byte(formattedName))
+		if err != nil {
+			return
+		}
+
+		if node.File.IsDir() {
+			printNodes(out, node.Children, parentPrefix+childPrefix)
+		}
+	}
+}
+
+func dirTree(out io.Writer, rootPath string, printFiles bool) error {
+	nodes, err := getDirNodes(rootPath, printFiles)
+	if err != nil {
+		return err
+	}
+
+	printNodes(out, nodes, "")
+
+	return nil
+}
 
 func main() {
 	out := os.Stdout
